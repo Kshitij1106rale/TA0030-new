aa2bbbf97c740169f2f9082894519e675e00a51b
Kshitij1106rale <harsharale@gmail.com>
2026-02-22 13:38:59 +0530

    Update menubar.tsx

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -5,7 +5,7 @@ import * as MenubarPrimitive from "@radix-ui/react-menubar"
 import { Check, ChevronRight, Circle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-
+ 
 function MenubarMenu({
   ...props
 }: React.ComponentProps<typeof MenubarPrimitive.Menu>) {
